450367bc3c7db25816f456f9c6a64cd2aef9a1b3
Aadesh Patel <aadeshpatel@Aadeshs-MacBook-Pro.local>
2015-01-17 12:42:44 -0500

    Fixed bug where each post was appearing twice in the output, Now reads keywords to look for off the keywords.json file and displays the results for each keyword separately

--- craig_scraper.py
+++ craig_scraper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-import sys, time, re, queue, threading, requests
-from urllib.request import urlopen
+import sys, time, re, queue, threading, requests, json, argparse
+from pprint import pprint
 from bs4 import BeautifulSoup
 from collections import namedtuple
+from cs_arg_parse import initArgParse
 
 # Post Tuple containing a post's Title and Url Extension
 Post = namedtuple("Post", ["title", "url_ext"])
@@ -12,21 +13,23 @@ class CraigsList:
     def __init__(self, city="sandiego"):
         self.city = city
         self.base_url = "https://" + self.city + ".craigslist.org/"
+        
+        """Parse JSON data from JSON file"""
+        with open("keywords.json") as file:
+            self.keywords = json.load(file)
+        
+        """Add a Queue key to each value in the JSON"""
+        for key, value in self.keywords.items():
+            value["posts"] = queue.PriorityQueue()
 
     def get_urls(self, search="sof"):
         """Gets and returns all the url extension for each post"""
         html = requests.get("http://sandiego.craigslist.org/search/%s?sort=rel" % search).text
         soup = BeautifulSoup(html)
-        return [a.attrs.get('href') for a in soup.select('div.content a[href*html]')]
+        return [a.attrs.get('href') for a in soup.select('div.content a.hdrlnk')]
 
     def query(self):
         """Creates a thread for each post to retrieve the data and then prints the overall data"""
-        self.keywords = {
-            "Java": {
-                "freq": 0,
-                "posts": queue.PriorityQueue()
-            },
-        }
 
         # Start a thread for each post and then join them into the main thread
         threads = [threading.Thread(target=self.collect_data, args=(url, self.keywords)) for url in self.get_urls()]
@@ -42,13 +45,16 @@ class CraigsList:
     def collect_data(self, url, keywords):
         """Determines if any of the search keywords appear in the post, if so it will increment the keyword's freq and enqueue a Post tuple"""
         html = requests.get(self.base_url + url).text
-        #html = urlopen(self.base_url + url).read()
         print("Scraping %s words from %s" % (len(html), url))
         soup = BeautifulSoup(html)
-
-        if soup.body.find(text=re.compile("Java")):
-            keywords["Java"]["freq"] += 1
-            keywords["Java"]["posts"].put(Post(soup.title.string, url))
+        
+        find = soup.body.find(text=re.compile("Java|Python"))
+        
+        if find:
+            for key, value in keywords.items():
+                if key in find:
+                    value["freq"] += 1
+                    value["posts"].put(Post(soup.title.string, url))
 
     def __str__(self):
         """
@@ -58,7 +64,7 @@ class CraigsList:
         output = []
 
         dash = []
-        [dash.append("-") for x in range(1, 50)]
+        [dash.append("-") for x in range(1, 96)]
         output.append("".join(dash))
 
         output.append("\n\n\033[1mBase Url: %s\033[0m\n" % (self.base_url))
@@ -67,16 +73,20 @@ class CraigsList:
         [searches.append(key) for key, value in self.keywords.items()]
         output.append("Searches: %s" % (", ".join(searches)))
 
-        output.append("\nNumber of Results: %s" % (self.keywords["Java"]["freq"]))
+        for key, value in self.keywords.items():
+            output.append("\n %s Results (%d)" % (key, value["freq"]))
+            output.append("".join(dash))
+            while not value["posts"].empty():
+                q = value["posts"].get()
+                output.append("{:<70} {:<40}".format(q.title, q.url_ext))
 
-        while not self.keywords["Java"]["posts"].empty():
-            q = self.keywords["Java"]["posts"].get()
-            output.append("{:<70} {:<40}".format(q.title, q.url_ext))
+            output.append("".join(dash))
 
         return "\n".join(output)
 
 if __name__ == "__main__":
     start = time.time()
+    p = initArgParse()
     c = CraigsList()
     c.query()
     end = time.time()

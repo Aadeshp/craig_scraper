200066a2146436ac5cf75ae26c0f06f92e694b44
Aadesh Patel <aadeshpatel@Aadeshs-MacBook-Pro.local>
2015-01-17 15:08:19 -0500

    added few more unix options
    Can now add keywords, delete all keywords, update region, and update
    search
    
    created superclass UpdateDataJson
    
    UpdateDataJson checks if .json file exists, if not it will create it.
    Subclasses of UpdateDataJson call the update_json method to add items or
    update the .json file

--- cs_arg_parse.py
+++ cs_arg_parse.py
@@ -2,10 +2,15 @@
 
 import argparse, json, os.path
 
-FILE_NAME = "keywords.json"
+FILE_NAME = "data.json"
 
-class AddKeyword(argparse.Action):
+class ShowKeywords(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
+        with open(FILE_NAME, "r") as file:
+            print(file.read())
+
+class UpdateDataJson(argparse.Action):
+    def update_json(self, update_key, values):
         if not os.path.isfile(FILE_NAME):
             self.create_json_file()
 
@@ -13,25 +18,48 @@ class AddKeyword(argparse.Action):
             data = json.load(file)
 
         with open(FILE_NAME, "w+") as file:
-            dict = {}
-            for value in values:
-                dict[value] = { "freq": 0 }
+            dict = data
+
+            if len(values) > 0:
+                for value in values:
+                    if update_key == "k":
+                        dict["Keywords"][value] = { "freq": 0 }
+                    elif update_key == "r":
+                        dict["Personal"]["Region"] = value
+                    elif update_key == "s":
+                        dict["Personal"]["Search"] = value
+            elif update_key == "rm":
+                dict["Keywords"] = {}
 
             data.update(dict)
-            file.write(json.dumps(data, indent=4, sort_keys=True))
-    
+            file.write(json.dumps(data, indent=4))
+
     def create_json_file(self):
         if not os.path.isfile(FILE_NAME):
             with open(FILE_NAME, "w") as file:
-                file.write("{\n}")
+                file.write("{\n\t\"Personal\": {\n\t\t\"Region\": \"INSERT REGION HERE\",\n\t\t\"Search\": \"INSERT SEARCH HERE\"\n\t},\n\t\"Keywords\": {\n\t}\n}")
 
-class ShowKeywords(argparse.Action):
+class AddKeyword(UpdateDataJson):
     def __call__(self, parser, namespace, values, option_string=None):
-        with open("keywords.json", "r") as file:
-            print(file.read())
+        self.update_json("k", values)
+
+class UpdateRegion(UpdateDataJson):
+    def __call__(self, parser, namespace, values, option_string=None):
+        self.update_json("r", values)
+
+class UpdateSearch(UpdateDataJson):
+    def __call__(self, parser, namespace, values, option_string=None):
+        self.update_json("s", values)
+
+class RemoveKeywords(UpdateDataJson):
+    def __call__(self, parser, namespace, values, option_string=None):
+        self.update_json("rm", values)
 
 def initArgParse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--add-keyword", action=AddKeyword, nargs='+')
+    parser.add_argument("--update-region", action=UpdateRegion, nargs=1)
+    parser.add_argument("--update-search", action=UpdateSearch, nargs=1)
+    parser.add_argument("--remove-keywords", action=RemoveKeywords, nargs=0)
     parser.add_argument("--keywords", action=ShowKeywords, nargs=0)
     return parser.parse_args()
